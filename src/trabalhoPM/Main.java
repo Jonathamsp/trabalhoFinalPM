5aaf048f251fdbc71a255dad1a6682d278e18e74
jonathamps <jotamno@hotmail.com>
2017-10-09 00:19:16 -0300

    PDF pronto

--- src/trabalhoPM/Main.java
+++ src/trabalhoPM/Main.java
@@ -1,11 +1,13 @@
 package trabalhoPM;
 
 public class Main {
+	private static final String CAMINHO_HISTORICO_ESCOLAR = "C:\\Users\\Jonatham\\Documents\\historico\\historicoEscolarCRAprovados.pdf";
+	
 	public static void main(String[] args) {
 		System.out.println("teste");
-		PdfParser testando = new PdfParser("C:\\Users\\Jonatham\\Documents\\historico\\historicoEscolarCRAprovados.pdf");
+		PdfParser testando = new PdfParser(CAMINHO_HISTORICO_ESCOLAR);
 		EscreverHtml writing = new EscreverHtml();
 		writing.writeHtml();
-		System.out.println(testando.getAllData());
+		System.out.println(testando.getAllData()[0]);
 	}
 }
